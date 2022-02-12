3ec7e243ba94d3f8734457a40ff95fe99dd016b3
Phantom <phantom@phantom-games.com>
2022-02-12 03:33:05 -0800

    Basic structures copied over from PMR-Tracker and stripped down to fit the needs of dog island

---
+++ scripts/main.js
@@ -0,0 +1,101 @@
+var urlParams = {};
+
+function getUrlParamCount() {
+    return Object.keys(urlParams).length;
+}
+
+function getUrlVars() {
+    if (getUrlParamCount() === 0) {
+        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+            urlParams[key.toLowerCase()] = value;
+        });
+    }
+    return urlParams;
+}
+
+function getUrlParam(parameter, defaultValue) {
+    var urlParameter = defaultValue;
+    if (parameter in urlParams) {
+        urlParameter = urlParams[parameter];
+    }
+    return urlParameter;
+}
+
+function localStorageGetWithDefault(key, defaultValue) {
+    const urlVal = getUrlParam(key, defaultValue);
+    if (urlVal === defaultValue) {
+        const value = localStorage.getItem(key);
+        if (!value) {
+            localStorage.setItem(key, defaultValue);
+            return defaultValue;
+        }
+        return value;
+    }
+    return urlVal;
+}
+
+$(document).ready(function(){
+    getUrlVars();
+
+    // disable some basic functionality
+    $('img').on('dragstart', function(){return false;});
+    $('html').contextmenu(function(){return false;});
+    $('img').contextmenu(function(){return false;});
+
+    $('.optional-item').height(50);
+    $('.badge').height(40);
+
+    // set text display for the main items
+    $('.section img').on('mouseenter', function() {
+        $(`#${$(this).attr("data-area")} h2`).text($(this).attr('data-name'));
+    });
+
+    $('.section img').on('mouseleave', function() {
+        $('.section h2').text("");
+    });
+
+    // bind individual item trackers
+    $('.optional-item').unbind("click").click(function() {
+        if ($(this).hasClass("unselected")) {
+            $(`img[data-name="${$(this).attr("data-name")}"]`).removeClass("unselected");
+        } else {
+            $(`img[data-name="${$(this).attr("data-name")}"]`).addClass("unselected");
+        }
+    });
+
+    // options menu
+    $(document).click(function(e) {
+        // if the option menu is open, and the click is outside the options menu, close it
+        var container = $("#options-menu");
+        if (container.hasClass("options-open") && !container.is(e.target) && container.has(e.target).length === 0) {
+            $("#options-menu-toggle").click();
+        }
+    });
+
+    $("#options-menu-toggle").click(function(e) {
+        e.stopPropagation();
+        $(this).toggleClass("options-open");
+        $("#options-menu").toggleClass("options-open");
+    });
+
+    $("#background-color").on("input", function() {
+        var color = $(this).val();
+        $("body, html").css("background-color", color);
+        localStorage.setItem("background-color", color);
+    });
+
+    $("#section-color").on("input", function() {
+        var color = $(this).val();
+        $(".section").css("background-color", color);
+        localStorage.setItem("section-color", color);
+    });
+
+    // local storage settings
+    var bg_color = localStorageGetWithDefault("background-color", "#24759e");
+    $("body, html").css("background-color", bg_color);
+    $("#background-color").val(bg_color);
+
+    var section_color = localStorageGetWithDefault("section-color", "#144e6c");
+    $(".section").css("background-color", section_color);
+    $("#section-color").val(section_color);
+});
